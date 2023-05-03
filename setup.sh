3ee3a2a15e8ffd339ce13dec43589244902bf52f
Jay Ovalle <jay.ovalle@gmail.com>
2023-05-02 20:32:01 -0400

    Add essentials

---
+++ setup.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+#
+# setup.sh - Deployment tool for Stargate
+# Released under Apache License 2.0.
+#
+# https://github.com/jovalle/stargate
+#
+
+VERSION="v1"
+
+#
+# Output usage information
+#
+
+usage() {
+  cat <<-EOF
+  Usage: setup.sh [options] [command]
+  Options:
+    -V, --version        output program version
+    -h, --help           output help information
+  Commands:
+    prepare              install required packages
+    deploy               create and start service
+    delete               stop and disable service
+EOF
+}
+
+#
+# Output fatal error
+#
+
+abort() {
+  echo
+  echo "  $@" 1>&2
+  echo
+  exit 1
+}
+
+#
+# Output version
+#
+
+version() {
+  echo $VERSION
+}
+
+#
+# Update/upgrade/install packages
+#
+
+prepare() {
+  apt update
+  apt upgrade -y
+  apt install -y \
+    curl \
+    dnsutils \
+    docker \
+    docker-compose \
+    net-tools \
+    rsync \
+    vim
+}
+
+#
+# Configure systemd unit
+#
+
+deploy() {
+  test -d /etc/stargate || abort "jovalle/stargate must reside in /etc/stargate"
+
+  if [[ ! -f /etc/systemd/system/stargate.service ]]; then
+    ln -s /etc/stargate/stargate.service /etc/systemd/system/stargate.service
+    popd
+  fi
+
+  test -f /etc/systemd/system/stargate.service && systemctl daemon-reload || abort "stargate.service not found"
+
+  systemctl status stargate &>/dev/null
+  if [[ $? -ne 0 ]]; then
+    echo "Stargate is NOT running. Restarting service..."
+    systemctl restart stargate
+  else
+    echo "Stargate is running"
+  fi
+
+  systemctl enable stargate
+}
+
+#
+# Stop and remove stargate services
+#
+
+delete() {
+  systemctl stop stargate
+  systemctl disable stargate
+  test -f /etc/systemd/system/stargate.service && rm -f /etc/systemd/system/stargate.service
+}
+
+#
+# Parse argv
+#
+
+while test $# -ne 0; do
+  arg=$1
+  shift
+  case $arg in
+    -h|--help) usage; exit ;;
+    -v|--version) version; exit ;;
+    prepare) prepare; ;;
+    deploy) deploy; ;;
+    delete) delete; ;;
+    *) usage; exit ;;
+  esac
+done
